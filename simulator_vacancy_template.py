5a7bc44275e7b871b66d8abfba4ec446078d6587
Samuel Begg <dal993192@g2-l-01.localdomain>
2025-09-16 15:01:22 -0500

    fixed bug where mean was not subtracted for Vmin. Confirmed results.

--- simulator_vacancy_template.py
+++ simulator_vacancy_template.py
@@ -31,7 +31,7 @@ alpha = 3.0
 samples = 640  #samples per batch
 batches = 10   #int(total_samples / samples)
 total_samples = samples * batches
-timesteps = 170
+timesteps = 200
 dt = 0.005 # save times 
 rtol = 10**(-7)
 atol = 10**(-10)
@@ -51,6 +51,11 @@ np.random.seed(disorder_seed)
 p = 0.0
 rand_draws = rand.uniform(0,1,Ninit)
 lattice_NVC_count = np.ones(Ninit,dtype=int)
+
+# Arbitrary angle correlator/variance
+maxNu = 10000
+nu_indices = np.arange(0,maxNu)
+nu = 2 * np.pi * nu_indices / maxNu 
 
 for ii in range(0,Ninit):
     if rand_draws[ii] < p:
@@ -72,7 +77,7 @@ hY_mat = hY * np.ones(N)
 hZ_mat = hZ * np.ones(N)
 
 # Construct a unique ID based on simulation parameters
-param_id = f"N{N}_alpha{alpha:.2f}_Jx{Jx:.2f}_Jy{Jy:.2f}_Jz{Jz:.2f}_hX{hX:.2f}_hY{hY:.2f}_hZ{hZ:.2f}_total_samples{total_samples}"
+param_id = f"N{N}_alpha{alpha:.2f}_Jx{Jx:.2f}_Jy{Jy:.2f}_Jz{Jz:.2f}_hX{hX:.2f}_hY{hY:.2f}_hZ{hZ:.2f}_total_samples{total_samples}_timesteps{timesteps}_dt{dt:.2f}"
 print(param_id)
 
 # File paths with unique suffix
@@ -112,24 +117,39 @@ S_init[:,0] = 1.0     # X position
 S_init[:,1] = 0.0     # Y position
 S_init[:,2] = 0.0     # Z position
 
-CorrZ_mean_batch = []
-CorrY_mean_batch = []
-CorrX_mean_batch = []
-CorrYZ_mean_batch = []
 
-CorrZ_std_batch = []
-CorrY_std_batch = []
-CorrX_std_batch = []
-CorrYZ_std_batch = []
+Sx_mean =  np.zeros([timesteps+1])
+Sy_mean =  np.zeros([timesteps+1])
+Sz_mean =  np.zeros([timesteps+1])
+Mxy_mean = np.zeros(timesteps+1)
 
-Sx_mean_batch = []
-Sy_mean_batch = []
-Sz_mean_batch = []
-Mxy_mean_batch = []
-Sx_std_batch = []
-Sy_std_batch = []
-Sz_std_batch = []
-Mxy_std_batch = []
+Sx_M2 =  np.zeros([timesteps+1])
+Sy_M2 =  np.zeros([timesteps+1])
+Sz_M2 =  np.zeros([timesteps+1])
+Mxy_M2 = np.zeros(timesteps+1)
+
+nSx = 0
+nSy = 0
+nSz = 0
+nMxy = 0
+
+nCorrX = 0
+nCorrY = 0
+nCorrZ = 0
+nCorrYZ = 0
+n_mat_Var = 0
+
+CorrX_mean =  np.zeros([timesteps+1])
+CorrY_mean =  np.zeros([timesteps+1]) 
+CorrZ_mean =  np.zeros([timesteps+1])
+CorrYZ_mean = np.zeros([timesteps+1])
+mat_Var_mean = np.zeros([maxNu,timesteps+1])
+
+CorrX_M2 =  np.zeros(timesteps+1)
+CorrY_M2 =  np.zeros(timesteps+1) 
+CorrZ_M2 =  np.zeros(timesteps+1)
+CorrYZ_M2 = np.zeros(timesteps+1)
+mat_Var_M2 = np.zeros([maxNu,timesteps+1])
 
 
 for bb in range(0,batches):
@@ -139,86 +159,74 @@ for bb in range(0,batches):
     Parallel(n_jobs=num_cores)(delayed(methods.dtwa_sc)(S_init, bb, ss, samples, timevec, N, Jx_mat, Jy_mat, Jz_mat, hX_mat, hY_mat, hZ_mat, temp_save_loc, rtol, atol) for ss in range(0,samples)) 
 
     # initialize matrices 
-    Sx_av =  np.zeros([N,timesteps+1])
-    Sy_av =  np.zeros([N,timesteps+1])
-    Sz_av =  np.zeros([N,timesteps+1])
+    Sx_av =  np.zeros(timesteps+1)
+    Sy_av =  np.zeros(timesteps+1)
+    Sz_av =  np.zeros(timesteps+1)
     Mxy_av = np.zeros(timesteps+1)
 
-    CorrZ_av =  np.zeros([N,N,timesteps+1])
-    CorrX_av =  np.zeros([N,N,timesteps+1])
-    CorrY_av =  np.zeros([N,N,timesteps+1]) 
-    CorrYZ_av = np.zeros([N,N,timesteps+1])
+    CorrZ_av =  np.zeros(timesteps+1)
+    CorrX_av =  np.zeros(timesteps+1)
+    CorrY_av =  np.zeros(timesteps+1) 
+    CorrYZ_av = np.zeros(timesteps+1)
 
 
     for ss in range(0,samples):
         sx_sample = np.load(temp_save_loc + "Sx_sample_" + str(ss) + ".npy")
         sy_sample = np.load(temp_save_loc + "Sy_sample_" + str(ss) + ".npy")
         sz_sample = np.load(temp_save_loc + "Sz_sample_" + str(ss) + ".npy")
+        
+        Sx_sum_t = np.sum(sx_sample,0)    
+        Sy_sum_t = np.sum(sy_sample,0)    
+        Sz_sum_t = np.sum(sz_sample,0)   
 
-        Sx_av += 1.0/ samples * sx_sample
-        Sy_av += 1.0/ samples * sy_sample
-        Sz_av += 1.0/ samples * sz_sample
+        Sx_av += 1.0/ samples * Sx_sum_t
+        Sy_av += 1.0/ samples * Sy_sum_t
+        Sz_av += 1.0/ samples * Sz_sum_t
         Mxy_av += 1.0/ samples * np.sqrt(np.sum(sx_sample,0)**2 + np.sum(sy_sample,0)**2)
 
-        CorrZ_av  += 1.0 / samples * np.einsum('nt,mt->nmt', sz_sample, sz_sample)
-        CorrX_av  += 1.0 / samples * np.einsum('nt,mt->nmt', sx_sample, sx_sample)
-        CorrY_av  += 1.0 / samples * np.einsum('nt,mt->nmt', sy_sample, sy_sample)
-        CorrYZ_av += 1.0 / samples * np.einsum('nt,mt->nmt', sy_sample, sz_sample)
+        CorrZ_av  += 1.0 / samples * Sz_sum_t**2
+        CorrX_av  += 1.0 / samples * Sx_sum_t**2
+        CorrY_av  += 1.0 / samples * Sy_sum_t**2  
+        CorrYZ_av += 1.0 / samples * Sy_sum_t * Sz_sum_t
 
         del sx_sample, sy_sample, sz_sample
 
-    # Add to batch lists
-    Sx_mean_batch += [Sx_av]
-    Sy_mean_batch += [Sy_av]
-    Sz_mean_batch += [Sz_av]
-    Mxy_mean_batch += [Mxy_av]
+    # Calculate the running mean and variance for magnetization and correlators
+    nSx, Sx_mean, Sx_std, Sx_M2 = methods.update_stats(Sx_av, nSx, Sx_mean, Sx_M2) 
+    nSy, Sy_mean, Sy_std, Sy_M2 = methods.update_stats(Sy_av, nSy, Sy_mean, Sy_M2) 
+    nSz, Sz_mean, Sz_std, Sz_M2 = methods.update_stats(Sz_av, nSz, Sz_mean, Sz_M2) 
+    nMxy, Mxy_mean, Mxy_std, Mxy_M2 = methods.update_stats(Mxy_av, nMxy, Mxy_mean, Mxy_M2) 
 
-    CorrZ_mean_batch +=  [CorrZ_av] 
-    CorrX_mean_batch += [CorrX_av] 
-    CorrY_mean_batch += [CorrY_av] 
-    CorrYZ_mean_batch += [CorrYZ_av] 
+    nCorrZ, CorrZ_mean, CorrZ_std, CorrZ_M2 = methods.update_stats(CorrZ_av, nCorrZ, CorrZ_mean, CorrZ_M2) 
+    nCorrX, CorrX_mean, CorrX_std, CorrX_M2 = methods.update_stats(CorrX_av, nCorrX, CorrX_mean, CorrX_M2) 
+    nCorrY, CorrY_mean, CorrY_std, CorrY_M2 = methods.update_stats(CorrY_av, nCorrY, CorrY_mean, CorrY_M2) 
+    nCorrYZ, CorrYZ_mean, CorrYZ_std, CorrYZ_M2 = methods.update_stats(CorrYZ_av, nCorrYZ, CorrYZ_mean, CorrYZ_M2) 
+
+    # Construct the expectation values to subtract from correlators in variance calc. Note: average conducted before product.
+    Sy_Sy_av = Sy_av**2
+    Sz_Sz_av = Sz_av**2  
+    Sy_Sz_av = Sy_av * Sz_av
+
+    ### Construct Variance for all possible angles in YZ plane
+    # Correlator
+    mat_Var_av = np.einsum("u,t->ut", np.cos(nu)**2 , CorrY_av) + np.einsum("u,t->ut", np.sin(nu)**2, CorrZ_av) - np.einsum("u,t->ut", 2 * np.sin(nu) * np.cos(nu), CorrYZ_av) 
+    #Subtract expectation values
+    mat_Var_av += -(np.einsum("u,t->ut", np.cos(nu)**2 , Sy_Sy_av) + np.einsum("u,t->ut", np.sin(nu)**2, Sz_Sz_av) - np.einsum("u,t->ut", 2 * np.sin(nu) * np.cos(nu), Sy_Sz_av)) 
+
+    # Calculate the running mean and variance of this
+    n_mat_Var, mat_Var_mean, mat_Var_std, mat_Var_M2 = methods.update_stats(mat_Var_av, n_mat_Var, mat_Var_mean, mat_Var_M2) 
 
 
-# Calculate averages and std over batches
-
-Sx_mean = np.mean(Sx_mean_batch,0)
-Sy_mean = np.mean(Sy_mean_batch,0)
-Sz_mean = np.mean(Sz_mean_batch,0)
-Mxy_mean = np.mean(Mxy_mean_batch,0)
-
-Sx_std = np.std(Sx_mean_batch,0)
-Sy_std = np.std(Sy_mean_batch,0)
-Sz_std = np.std(Sz_mean_batch,0)
-Mxy_std = np.std(Mxy_mean_batch,0)
-
-CorrZ_mean = np.mean(CorrZ_mean_batch,0)
-CorrX_mean = np.mean(CorrX_mean_batch,0)
-CorrY_mean = np.mean(CorrY_mean_batch,0)
-CorrYZ_mean = np.mean(CorrYZ_mean_batch,0)
-
-CorrZ_std = np.std(CorrZ_mean_batch,0)
-CorrX_std = np.std(CorrX_mean_batch,0)
-CorrY_std = np.std(CorrY_mean_batch,0)
-CorrYZ_std = np.std(CorrYZ_mean_batch,0)
-
-endTime = datetime.now()
-print(str(endTime - startTime),'Run time')
-
-# Arbitrary angle correlator/variance
-maxNu = 10000
+    endTime = datetime.now()
+    print(str(endTime - startTime),'Batch run time')
 
 Vmin_mean = np.zeros(np.size(timevec))
 Vmin_std = np.zeros(np.size(timevec))
 
-nu_indices = np.arange(0,maxNu)
-nu = 2 * np.pi * nu_indices / maxNu 
-
-temp_VMin = np.einsum("u,st->ust", np.cos(nu)**2 , np.sum(np.sum(CorrY_mean_batch,1),1)) + np.einsum("u,st->ust", np.sin(nu)**2, np.sum(np.sum(CorrZ_mean_batch,1),1)) - np.einsum("u,st->ust", 2 * np.sin(nu) * np.cos(nu), np.sum(np.sum(CorrYZ_mean_batch,1),1)) 
-
 for tt in range(0,np.size(timevec)):
 
-    Vmin_nu = np.mean(temp_VMin[:,:,tt],1)
-    Vmin_nu_std = np.std(temp_VMin[:,:,tt],1)
+    Vmin_nu = mat_Var_mean[:,tt]
+    Vmin_nu_std = mat_Var_std[:,tt]
     arg = np.argmin(Vmin_nu)
     Vmin_mean[tt] = Vmin_nu[arg]
     Vmin_std[tt] = Vmin_nu_std[arg]
@@ -234,14 +242,17 @@ np.save(loc + "Sx_std.npy",Sx_std)
 np.save(loc + "Sz_std.npy",Sz_std)
 np.save(loc + "Mxy.npy",Mxy_mean)
 np.save(loc + "Mxy_std.npy",Mxy_std)
-np.save(loc + "SySz.npy",np.sum(np.sum(CorrYZ_mean,0),0))
-np.save(loc + "SySz_std.npy",np.sum(np.sum(CorrYZ_std,0),0))
-np.save(loc + "SySy.npy",np.sum(np.sum(CorrY_mean,0),0))
-np.save(loc + "SySy_std.npy",np.sum(np.sum(CorrY_std,0),0))
-np.save(loc + "SxSx.npy",np.sum(np.sum(CorrX_mean,0),0))
-np.save(loc + "SxSx_std.npy",np.sum(np.sum(CorrX_std,0),0))
-np.save(loc + "SzSz.npy",np.sum(np.sum(CorrZ_mean,0),0))
-np.save(loc + "SzSz_std.npy",np.sum(np.sum(CorrZ_std,0),0))
+np.save(loc + "SySz.npy", CorrYZ_mean)
+np.save(loc + "SySz_std.npy", CorrYZ_std)
+np.save(loc + "SySy.npy",CorrY_mean)
+np.save(loc + "SySy_std.npy",CorrY_std)
+np.save(loc + "SxSx.npy",CorrX_mean)
+np.save(loc + "SxSx_std.npy",CorrX_std)
+np.save(loc + "SzSz.npy",CorrZ_mean)
+np.save(loc + "SzSz_std.npy",CorrZ_std)
 np.save(loc + "timevec.npy",timevec)
 np.save(loc + "Vmin.npy",Vmin_mean)
 np.save(loc + "Vmin_std.npy",Vmin_std)
+
+endTime = datetime.now()
+print(str(endTime - startTime),'Total run time')
